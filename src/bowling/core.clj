40e7e8cf1bfd5515505e036563f9adba7e8f6d46
Thomas Crowley <crowley@kibu.com.au>
2016-11-23 11:45:27 +1100

    Sketch out skeloton impl

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -1,6 +1,25 @@
-(ns bowling.core)
+(ns bowling.core
+  "Ten-pin bowling game scorecard.
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  Build an API that implements a ten-pin bowling scorecard"
+  (:require [clojure.spec :as s]))
+
+;; -- spec ----------------------------------------------------------------------------------------
+
+(s/def ::scorecard any?)
+
+(s/def ::result any?)
+
+;; -- impl ----------------------------------------------------------------------------------------
+
+;; Create an empty score card
+(defn scorecard []
+  {})
+
+;; Given a score card, score a frame
+(defn score-frame [card result]
+  card)
+
+;; Determine if a game is complete - if so, provide the final score
+(defn game-complete? []
+  true)
